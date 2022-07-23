c0eb72430702b8f49b94f1748416f0206eb5da46
HazWahb8080 <hazem.wahb100@gmail.com>
2022-07-23 12:50:43 +0200

    added--- signin handling sessions landing

--- pages/dash.js
+++ pages/dash.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useEffect, useState } from "react";
 import Header from "./../components/Header";
 import { useRouter } from "next/router";
@@ -12,6 +13,7 @@ import {
 import { db } from "../firebase";
 import { useSession } from "next-auth/react";
 import Appointment from "./../components/Appointment";
+import { imageLanding } from './../utils/images/imgsDb';
 
 function Dash() {
   const router = useRouter();
@@ -31,32 +33,43 @@ function Dash() {
     }
   }, [usermail]);
 
-  return (
-    <div className="w-full items-center h-full justify-center flex flex-col space-y-6">
-      { router.pathname === "/dash" && <Header/>}
-      {/*created appointments  list */}
-      <div className="flex items-center justify-center w-full ">
-        {loading ? (
-          <div className="w-full h-screen items-center justify-center flex flex-col">
-            loading
-          </div>
-        ) : (
-          <div
-            className="w-full lg:w-3/4 items-center justify-center flex 
-          flex-col h-fit overflow-y-scroll scrollbar-hide  border-b px-4 py-4 border-black/50"
-          >
-            {appointments.map((appointment) => (
-              <Appointment
-                key={appointment.id}
-                data={appointment.data()}
-                id={appointment.id}
-              />
-            ))}
-          </div>
-        )}
+  if (!usermail) {
+    return (
+      <div className="w-full items-center h-screen justify-center flex flex-col space-y-6">
+        <div className="flex items-center h-full justify-center w-full ">
+          <img src={imageLanding.title} alt={imageLanding.alt} className="w-[250px] object-cover object-center" />
+          Please Sign in First
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="w-full items-center h-full justify-center flex flex-col space-y-6">
+        {router.pathname === "/dash" && <Header />}
+        {/*created appointments  list */}
+        <div className="flex items-center justify-center w-full ">
+          {loading ? (
+            <div className="w-full h-screen items-center justify-center flex flex-col">
+              loading
+            </div>
+          ) : (
+            <div
+              className="w-full lg:w-3/4 items-center justify-center flex 
+          flex-col h-fit overflow-y-scroll scrollbar-hide  border-b px-4 py-4 border-black/50"
+            >
+              {appointments.map((appointment) => (
+                <Appointment
+                  key={appointment.id}
+                  data={appointment.data()}
+                  id={appointment.id}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dash;
---
+++ utils/images/imgsDb.js
@@ -0,0 +1,4 @@
+export const imageLanding = {
+    title:"https://ucarecdn.com/4aefb512-969d-443e-972c-e69c9de71fca/experimentalstickerexclamation.png",
+    alt:"landing image"
+};
